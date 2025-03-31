62a8e45565bdb02db7af10a5c5aeb5713d8e0f74
pabloalosan <202214595@alu.comillas.edu>
2025-03-31 23:24:59 +0200

    Esqueleto

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/MotocicletasApiApplication.java
@@ -0,0 +1,14 @@
+package edu.comillas.icai.gitt.pat.spring.mvc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotocicletasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotocicletasApiApplication.class, args);
+    }
+
+}
